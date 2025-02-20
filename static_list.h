d8514dc10790a6cbc97e0f683a6e2ca684ae2f55
 <ali.advantage26@gmail.com>
2025-02-20 11:10:29 +0100

    Añadi comentario login correcto

--- static_list.h
+++ static_list.h
@@ -1,11 +1,12 @@
 /*
- * TITLE: PROGRAMMING II LABS
- * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
- */
+TITLE: PROGRAMMING II LABS                   SUBTITLE: P0
+
+AUTHOR 1: Ali Abu-afash Nayef  LOGIN 1: ali.nayef@udc.es
+
+AUTHOR 2: Pedro Rodríguez Raposo  LOGIN 2: pedro.rodriguez.raposo@udc.es
+
+GROUP: 1.2                                                        DATE: 20/02/2025
+*/
 
 #ifndef STATIC_LIST_H
 #define STATIC_LIST_H
