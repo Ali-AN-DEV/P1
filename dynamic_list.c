60726d18772daa8454638421fe4b726f9a9a729e
 <ali.advantage26@gmail.com>
2025-03-06 11:09:32 +0100

    Listas dinámicas - empezar

--- dynamic_list.c
+++ dynamic_list.c
@@ -1,11 +1,12 @@
 /*
- * TITLE: PROGRAMMING II LABS
- * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
- */
+TITLE: PROGRAMMING II LABS                   SUBTITLE: P1
+
+AUTHOR 1: Ali Abu-afash Nayef  LOGIN 1: ali.nayef@udc.es
+
+AUTHOR 2: Pedro Rodríguez Raposo  LOGIN 2: pedro.rodriguez.raposo@udc.es
+
+GROUP: 1.2                                                        DATE: 05/03/2025
+*/
 
 #include "dynamic_list.h"
 
